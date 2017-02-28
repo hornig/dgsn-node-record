4fea566a2ebf253f114918fbe7084a20028337d3
hornig <chefkueche@hornshouse.com>
2017-02-28 19:59:12 +0100

    no stick, no record

--- software/main.py
+++ software/main.py
@@ -3,7 +3,7 @@ import platform
 #import requests
 #import numpy as np
 #import sys
-from rtlsdr import RtlSdr
+from rtlsdr import RtlSdr, librtlsdr
 
 import hashlib
 from uuid import getnode as get_mac
@@ -13,6 +13,30 @@ import time
 
 # import multiprocessing
 from multiprocessing import Process, Lock
+
+
+def create_config():
+    sdrconfig = { "sdr":
+            [
+                {
+                    "name": "rtlsdr",
+                    "type": "rtlsdr",
+                    "center_frequency": 1090000000,
+                    "samplerate": 2000000,
+                    "gain_fixed": "auto",
+                    "gain_auto_range": [0, 50],
+                    "gain_auto_threshold": 0.3,
+                    "frequency_correction": 0,
+                    "mode": "",
+                    "timer": [100000, 200000],
+                    "recording_time": 90
+                }
+            ]
+            }
+
+    # Writing JSON data
+    with open('sdr_config.json', 'w') as f:
+        json.dump(sdrconfig, f, indent=4)
 
 
 def do_sha224(x):
@@ -43,37 +67,42 @@ def get_groundstationid():
 
 def storing_stream_with_windows(lock, rs, cf, gain, ns, device, path_storing):
 
-    lock.acquire()
+    if librtlsdr.rtlsdr_get_device_count() > 0:
+        lock.acquire()
+        sdr = RtlSdr(device_index = device)
 
-    sdr = RtlSdr(device_index = device)
+        # some defaults
+        sdr.rs = rs
+        sdr.fc = cf
+        sdr.gain = gain
 
-    # some defaults
-    sdr.rs = rs
-    sdr.fc = cf
-    sdr.gain = gain
+        samples = sdr.read_bytes(ns * 2)
+        sdr.close()
 
-    samples = sdr.read_bytes(ns * 2)
-    sdr.close()
-
-    lock.release()
+        lock.release()
     #print("print")
 
-    filename = get_groundstationid() + "_f" + str(cf) + "_d" + str(device) + "_t" + str(int(time.time()))
-    f = open(path_storing + filename, 'wb')
-    f.write(samples)
-    f.close()
-    os.rename(path_storing + filename, path_storing + filename + ".dat")
+        filename = get_groundstationid() + "_f" + str(cf) + "_d" + str(device) + "_t" + str(int(time.time()))
+        f = open(filename, 'wb')
+        f.write(samples)
+        f.close()
+        os.rename(path_storing + filename, path_storing + filename + ".dat")
 
-def run(path_storing, device):
+def run(path_storing, path_ops, path_logs, device):
     print("you are using", platform.system(), platform.release(), os.name)
 
     print("your groundstation id is", get_groundstationid())
 
+    print("loading configs from", path_ops + "sdr_config.json", "for device", device)
+    with open(path_ops + 'sdr_config.json', 'r') as f:
+         sdr_configuration = json.load(f)
+    print(sdr_configuration)
+
     #device = 0
-    rs = 2000000
-    cf = 1090000000
-    ns = rs * 20
-    gain = "auto"
+    rs   = sdr_configuration["sdr"][0]["samplerate"]
+    cf   = sdr_configuration["sdr"][0]["center_frequency"]
+    ns   = sdr_configuration["sdr"][0]["recording_time"] * rs
+    gain = sdr_configuration["sdr"][0]["gain_fixed"]
 
     #print(test.update(nsamples))
 
@@ -101,6 +130,9 @@ def run(path_storing, device):
                     print("rec number", recs, 'added')
 
 if __name__ == '__main__':
+    #create_config()
     device = 0
     path_storing = ""
-    run(path_storing, device)
+    path_ops = ""
+    path_logs = ""
+    run(path_storing, path_ops, path_logs, device)
