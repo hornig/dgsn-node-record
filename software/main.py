7b6f21636ec7d0cc0d3c5d04a2d20e87b06a73f6
hornig <chefkueche@hornshouse.com>
2017-04-08 00:24:00 +0200

    timer #2

--- software/main.py
+++ software/main.py
@@ -25,7 +25,8 @@ def create_config():
                     "gain_auto_threshold": 0.3,
                     "frequency_correction": 0,
                     "mode": "",
-                    "timer": ["2017-04-01 11:00:00.0", "2017-04-01 11:15:00.0"],
+                    "record_timer": [["2017-04-07 20:50:00.0", "2017-04-07 20:51:00.0"]],
+                    "record_now" : 0,
                     "recording_time": 90
                 }
             ]
@@ -74,19 +75,25 @@ def storing_stream_with_windows(lock, rs, cf, gain, ns, device, path_storing):
         sdr.fc = cf
         sdr.gain = gain
 
+        timestamp = time.time()
         samples = sdr.read_bytes(ns * 2)
         sdr.close()
 
         lock.release()
     #print("print")
 
-        filename = get_groundstationid() + "_f" + str(cf) + "_d" + str(device) + "_t" + str(int(time.time()))
+        filename = get_groundstationid() + "_f" + str(cf) + "_d" + str(device) + "_t" + str(int(timestamp))
         f = open(path_storing + filename + ".tmp", 'wb')
         f.write(samples)
         f.close()
         os.rename(path_storing + filename + ".tmp", path_storing + filename + ".dat")
 
 def convert_time(s):
+
+    # sometimes, the time did not have a decimal part and the dateconvert did not work
+    if s.find(".") == -1:
+        s = s + ".0"
+
     return time.mktime(datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S.%f").utctimetuple())
 
 def check_rtl_device(device):
@@ -98,14 +105,18 @@ def check_rtl_device(device):
         print("rtlsdr device", device, "not found")
 
 
+def load_sdr_input():
+    with open(path_ops + 'sdr_config.json', 'r') as f:
+         sdr_configuration = json.load(f)
+    return sdr_configuration
+
 def run(path_storing, path_ops, path_logs, device):
     print("you are using", platform.system(), platform.release(), os.name)
 
     print("your groundstation id is", get_groundstationid())
 
     print("loading configs from", path_ops + "sdr_config.json", "for device", device)
-    with open(path_ops + 'sdr_config.json', 'r') as f:
-         sdr_configuration = json.load(f)
+    sdr_configuration = load_sdr_input()
     print(sdr_configuration)
 
     #device = 0
@@ -113,20 +124,29 @@ def run(path_storing, path_ops, path_logs, device):
     cf   = sdr_configuration["sdr"][0]["center_frequency"]
     ns   = sdr_configuration["sdr"][0]["recording_time"] * rs
     gain = sdr_configuration["sdr"][0]["gain_fixed"]
-    timer= sdr_configuration["sdr"][0]["timer"]
+    record_timer = sdr_configuration["sdr"][0]["record_timer"]
 
-    timer[0] = convert_time(timer[0])
-    timer[1] = convert_time(timer[1])
+
+    timer =[[],[]]
+    timer[0] = convert_time(record_timer[0][0])
+    timer[1] = convert_time(record_timer[0][1])
     print(timer, convert_time(str(datetime.datetime.utcfromtimestamp(time.time()))))
 
+    check_rtl_device(device)
     time_wait = 10.0
-    while timer[0] > convert_time(str(datetime.datetime.utcfromtimestamp(time.time()))):
+    while timer[0] > convert_time(str(datetime.datetime.utcfromtimestamp(time.time()))) and \
+                    sdr_configuration["sdr"][0]["record_now"] != 1:
+
+        sdr_configuration = load_sdr_input()
+        print(sdr_configuration)
+
         if timer[0] - convert_time(str(datetime.datetime.utcfromtimestamp(time.time()))) > time_wait:
             time_countdown = time_wait
         else:
             time_countdown = timer[0] - convert_time(str(datetime.datetime.utcfromtimestamp(time.time())))
 
-        check_rtl_device(device)
+        print("waiting", time_countdown)
+        #check_rtl_device(device)
         print(timer[0] - convert_time(str(datetime.datetime.utcfromtimestamp(time.time()))), time_countdown)
         time.sleep(time_countdown)
 
@@ -139,13 +159,16 @@ def run(path_storing, path_ops, path_logs, device):
         jobs = []
 
         # initializing the recording instances
-        for recs in range(2):
-            p = Process(target=storing_stream_with_windows, args=(lock, rs, cf, gain, ns, device, path_storing))
-            jobs.append(p)
-            p.start()
-            #print("end")
+        if timer[1] >= convert_time(str(datetime.datetime.utcfromtimestamp(time.time()))) or timer[0] == timer[1] or \
+                        sdr_configuration["sdr"][0]["record_now"] == 1:#True == True:
+            for recs in range(2):
+                p = Process(target=storing_stream_with_windows, args=(lock, rs, cf, gain, ns, device, path_storing))
+                jobs.append(p)
+                p.start()
+                #print("end")
 
-        while timer[1] >= convert_time(str(datetime.datetime.utcfromtimestamp(time.time()))) or timer[0] == timer[1]:#True == True:
+        while timer[0] == timer[1] or timer[1] >= convert_time(str(datetime.datetime.utcfromtimestamp(time.time()))) and \
+                        sdr_configuration["sdr"][0]["record_now"] == 1:#True == True:
             sleeping_time = 2
             #time.sleep(sleeping_time)
             for n, p in enumerate(jobs):
@@ -156,6 +179,10 @@ def run(path_storing, path_ops, path_logs, device):
                     jobs.append(p)
                     p.start()
                     print("rec number", recs, 'added', lock)
+
+            sdr_configuration = load_sdr_input()
+            print(sdr_configuration)
+            time.sleep(10)
 
 if __name__ == '__main__':
     #create_config()
